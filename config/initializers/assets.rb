b95cae7b263d999a70bae19f3ad011d61ecbf31c
0084takuya <ba-bo-lat.ot-fn.109-110.5296@ezweb.ne.jp>
2020-07-17 23:04:46 +0900

    fix bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+[File.join('app', 'assets', 'stylesheets'), File.join('app', 'assets', 'javascripts')].each do |dir|
+  Dir.glob("#{dir}/controllers/**/*")
+    .select {|p| p =~ /\.(js|css|scss|erb)$/ && p.exclude?('/_') }
+    .map {|p| p.gsub(/\.s?(css|js)(\.erb|)$/, '.\1').gsub("#{dir}/", '') }
+    .each do |file|
+      Rails.application.config.assets.precompile += [file]
+    end
+end
